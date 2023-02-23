c8ba340850fd9c02057c8c813cd436a7ba5075e3
swagless-monk <72817022+swagless-monk@users.noreply.github.com>
2023-02-23 13:48:29 -0500

    Create more simple code for emotion detection

--- fer.py
+++ fer.py
@@ -1,24 +1,5 @@
-from warnings import filterwarnings
-from os import listdir
-from fer import FER
-from facenet_pytorch import MTCNN
-from cv2 import imread, imshow
+from fer_pytorch.fer import FER
 
-image_path = './images/faces/'
-
-for image in listdir(image_path):
-    print(image)
-    image = imread(image_path + image)
-
-    emotion_detector = FER(mtcnn=True)
-    emotions = emotion_detector.detect_emotions(image)
-
-    emo_dict = emotions[0]['emotions']
-
-    emo = 0
-    for val in emo_dict.values():
-        if val > emo:
-            emo = val
-
-    facial_emotion = list(emo_dict.keys())[list(emo_dict.values()).index(emo)]
-    print(f'EMOTION: {facial_emotion.capitalize()} ({emo*100} %)', end='\n\n')
+fer = FER()
+fer.get_pretrained_model("resnet34")
+fer.run_webcam()
